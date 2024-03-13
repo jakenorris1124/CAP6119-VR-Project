7f4b78a71aaeed0634ec75ee15728cb1e2202ff1
Jake Norris <jakenorris1124@gmail.com>
2024-03-13 18:56:21 -0400

    Added function to clamp gravity direction so gravity isnt at a strange angle

--- Assets/Scripts/GravityManager.cs
+++ Assets/Scripts/GravityManager.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Authentication.ExtendedProtection;
+using Unity.VisualScripting;
 using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -8,11 +10,53 @@ using UnityEngine.InputSystem;
 public class GravityManager : MonoBehaviour
 {
     [SerializeField] private GameObject XROrigin;
-    
-    public void ChangeGravity(Vector3 direction)
+
+    private Vector3[] cardinals = new Vector3[]
     {
-       Physics.gravity = direction;
-       StartCoroutine(RotatePlayer(direction));
+        Vector3.up,
+        Vector3.down,
+        Vector3.left,
+        Vector3.right,
+        Vector3.forward, 
+        Vector3.back
+    };
+    
+    public void ChangeGravity(Vector3 direction, bool clamp = true)
+    {
+        if (clamp)
+            direction = Clamp(direction);
+        
+        Physics.gravity = direction;
+        StartCoroutine(RotatePlayer(direction));
+    }
+
+    /// <summary>
+    /// Clamps the input vector to the nearest cardinal direction
+    /// </summary>
+    /// <param name="direction">The vector to clamp</param>
+    /// <example>If a Vector3(0.4, 0.3, 0.9) was input, then a Vector3(0, 0, 1) would
+    /// be returned</example>
+    /// <returns>The nearest cardinal direction to the input value.</returns>
+    private static Vector3 Clamp(Vector3 direction)
+    {
+        Vector3 strict = direction.Abs();
+
+        if (strict.x > strict.y && strict.x > strict.z)
+            return new Vector3(1 * GetSign(direction.x), 0, 0);
+        if (strict.y > strict.x && strict.y > strict.z)
+            return new Vector3(0, 1 * GetSign(direction.y), 0);
+        
+        return new Vector3(0, 0, 1 * GetSign(direction.z));
+    }
+
+    /// <summary>
+    /// Gets the sign of <paramref name="value"/>
+    /// </summary>
+    /// <param name="value">The value to check the sign of</param>
+    /// <returns>1 if <paramref name="value"/> is positive, -1 if it is negative</returns>
+    private static int GetSign(float value)
+    {
+        return value > 0 ? 1 : -1;
     }
 
     private IEnumerator RotatePlayer(Vector3 direction)
