af132f7877f2aa0ee83e1f65636a241461fbfb33
Jake Norris <jakenorris1124@gmail.com>
2024-03-26 20:37:48 -0400

    Switched from character controller to rigid body physics

--- Assets/Scripts/GravityManager.cs
+++ Assets/Scripts/GravityManager.cs
@@ -4,24 +4,46 @@ using System.Collections.Generic;
 using System.Security.Authentication.ExtendedProtection;
 using Unity.VisualScripting;
 using Unity.XR.CoreUtils;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class GravityManager : MonoBehaviour
 {
     [SerializeField] private GameObject XROrigin;
+    private Rigidbody _rigidbody;
+    private SafeZone _playerSafetyManager;
+    
+    enum Axis
+    {
+        X, Y, Z
+    }
+
+    private Axis _currentAxis;
+    
     
     /// <summary>
     /// Time it takes to rotate the player once gravity is changed in seconds.
     /// </summary>
     [SerializeField] private float rotationTime = 1f;
-    
+
+    private void Start()
+    {
+        _rigidbody = XROrigin.GetComponent<Rigidbody>();
+        _playerSafetyManager = XROrigin.GetComponent<SafeZone>();
+        _currentAxis = Axis.Y;
+
+        _rigidbody.constraints = ConstrainedAxis();
+    }
+
     public void ChangeGravity(Vector3 direction, bool clamp = true)
     {
         if (clamp)
             direction = Clamp(direction);
         
         Physics.gravity = direction;
+        UpdateCurrentAxis(direction);
+        
         StartCoroutine(RotatePlayer(direction));
     }
 
@@ -54,15 +76,42 @@ public class GravityManager : MonoBehaviour
         return value > 0 ? 1 : -1;
     }
 
+    private void UpdateCurrentAxis(Vector3 direction)
+    {
+        if (direction.x != 0)
+            _currentAxis = Axis.X;
+        else if (direction.y != 0)
+            _currentAxis = Axis.Y;
+        else
+            _currentAxis = Axis.Z;
+    }
+
     private IEnumerator RotatePlayer(Vector3 direction)
     {
         Vector3 up = XROrigin.transform.up;
-        Vector3 inverted = new Vector3(direction.x * -1, direction.y * -1, direction.z * -1);
+        Vector3 inverted = direction * -1;
 
+        // Unlock rigidbody rotation
+        _rigidbody.constraints = RigidbodyConstraints.None;
+        
         for (var elapsedTime = 0f; elapsedTime <= rotationTime; elapsedTime += Time.deltaTime)
         {
             XROrigin.transform.up = Vector3.Slerp(up, inverted, elapsedTime);
             yield return null;
         }
+        
+        // Lock rigidbody rotation
+        _rigidbody.inertiaTensorRotation = XROrigin.transform.rotation;
+        _rigidbody.constraints = ConstrainedAxis();
+        
+        _playerSafetyManager.TrySafetyFix();
     }
+
+    private RigidbodyConstraints ConstrainedAxis() => _currentAxis switch
+    {
+        Axis.X => RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ,
+        Axis.Y => RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ,
+        Axis.Z => RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY,
+        _ => throw new ArgumentOutOfRangeException(nameof(_currentAxis), _currentAxis, null)
+    };
 }
