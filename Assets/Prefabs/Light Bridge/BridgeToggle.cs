f6371c65a4fb7f7b2973bc2c1995cfc560841724
Jake Norris <jakenorris1124@gmail.com>
2024-04-16 01:30:53 -0400

    Mostly finished level 2

--- Assets/Prefabs/Light Bridge/BridgeToggle.cs
+++ Assets/Prefabs/Light Bridge/BridgeToggle.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,7 +9,33 @@ public class BridgeToggle : MonoBehaviour
 
     [SerializeField] private AudioSource on;
     [SerializeField] private AudioSource off;
+
+    [SerializeField] private bool startDisabled;
+
+    private GameObject _passthrough;
+    private MeshRenderer _passthroughMesh;
     
+    private GameObject _solid;
+    private MeshRenderer _solidMesh;
+    private MeshCollider _solidCollider;
+
+    private void Start()
+    {
+        _passthrough = gameObject.transform.Find("Passthrough").gameObject;
+        _passthroughMesh = _passthrough.GetComponent<MeshRenderer>();
+        
+        
+        _solid = gameObject.transform.Find("Solid").gameObject;
+        _solidMesh = _solid.GetComponent<MeshRenderer>();
+        _solidCollider = _solid.GetComponent<MeshCollider>();
+
+        if (startDisabled)
+        {
+            state = false;
+            Off(false);
+        }
+    }
+
 
     public void Toggle()
     {
@@ -19,16 +46,28 @@ public class BridgeToggle : MonoBehaviour
         else
             Off();
     }
-    
-    public void Off()
+
+    public void Off(bool sound = true)
     {
-        off.Play();
-        gameObject.SetActive(false);
+        if (sound)
+            off.Play();
+
+
+        _passthroughMesh.enabled = false;
+
+        _solidMesh.enabled = false;
+        _solidCollider.enabled = false;
     }
 
     public void On()
     {
         gameObject.SetActive(true);
+        
+        _passthroughMesh.enabled = true;
+
+        _solidMesh.enabled = true;
+        _solidCollider.enabled = true;
+        
         on.Play();
     }
 }
